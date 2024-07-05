2f5f583c8acf59b80f67beccb9694c7ebf8182a3
Mohamad Septiawan <septi53@gmail.com>
2024-07-05 09:37:54 +0700

    Tambah lagi nih

--- Tantang.py
+++ Tantang.py
@@ -6,4 +6,9 @@ nilaiAkhir = 25
 while (nilaiAwal <= nilaiAkhir):
     hasil = "Prima" if is_prima(nilaiAwal) else "Bukan Prima"
     print(nilaiAwal, hasil)
+    daftar = [nilaiAwal, hasil]
     nilaiAwal = nilaiAwal + 1
+
+fileHasil = open("hasil.txt", "w")
+fileHasil.write(daftar)
+fileHasil.close()
